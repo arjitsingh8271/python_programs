2cdad87518834ccc157e0e50e6a0403e65895fa0
Arjit Kumar Singh <78999136+arjitsingh8271@users.noreply.github.com>
2022-09-14 10:39:05 +0530

    aa.py

--- aa.py
+++ aa.py
@@ -1,6 +1,6 @@
 my_dict = {"Name":["Arjit","Ravi","Rahul"],
-					 "Roll":[71,45,66],
-					 "Branch":["BCA","BBA","MBA"]}
+	   "Roll":[71,45,66],
+	   "Branch":["BCA","BBA","MBA"]}
 
 print(my_dict["Name"][1])
 print(my_dict["Roll"][2])
