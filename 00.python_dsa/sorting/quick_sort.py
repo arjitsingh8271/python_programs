641107b0e576edc8dca5a0e5d6dae4f9e53907a0
arjitsingh8271 <arjitkumarsingh@gmail.com>
2024-04-29 21:00:24 +0530

    update

--- 00.python_dsa/sorting/quick_sort.py
+++ 00.python_dsa/sorting/quick_sort.py
@@ -1,3 +1,4 @@
+"""
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr
@@ -21,4 +22,40 @@ for i in range(size):
 
 print(list1)
 quick_sort(list1)
+print(list1)
+"""
+
+
+def partion(ll,lb,up):
+    pivot=ll[lb]
+    i=lb
+    j=up
+    while i<j:
+        while i<=j and ll[i]<=pivot:
+            i=i+1
+        while i<=j and ll[j]>=pivot:
+            j=j-1
+        if i<j:
+            ll[i],ll[j]=ll[j],ll[i]
+    ll[lb],ll[j]=ll[j],ll[lb]
+    return(j)
+
+def quick_sort(ll,start,end):
+    if start>=end:
+        return
+    p=partion(ll,start,end)
+    quick_sort(ll,start,p-1)
+    quick_sort(ll,p+1,end)
+
+
+size = int(input("Enter Size: "))
+
+list1 = []
+
+for i in range(size):
+    ele = int(input())
+    list1.append(ele)
+
+print(list1)
+quick_sort(list1, 0, len(list1)-1)
 print(list1)
